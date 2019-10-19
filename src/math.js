97af49437ea9175564a852f7e118eada2ce5fde8
annapuchkova <puchkova.anne@gmail.com>
2019-10-19 15:10:36 +0400

    add homework lesson2

---
+++ src/math.js
@@ -0,0 +1,12 @@
+export const getSum = (a, b) => {
+    return a + b;
+};
+export const getDiff = (a, b) => {
+    return a - b;
+};
+export const getProd = (a, b) => {
+    return a * b;
+};
+export const getQuot = (a, b) => {
+    return a / b;
+};
