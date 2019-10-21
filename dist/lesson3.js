4ac981f9e329ce500ebe557d2a26a70c7fe69fa2
annapuchkova <puchkova.anne@gmail.com>
2019-10-21 23:29:25 +0400

    HW lesson3

--- dist/lesson3.js
+++ dist/lesson3.js
@@ -59,3 +59,104 @@ var compareArrs = function compareArrs(a, b) {
     return result;
   }
 };
+
+// Task 2
+// С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
+var getAnswerTask1 = function getAnswerTask1() {
+  var n = 0;
+  var result = [];
+  while (n <= 100) {
+    if (isPrime(n)) {
+      result = [].concat(_toConsumableArray(result), [n]);
+    }
+    n++;
+  }
+  document.getElementById('task1').innerHTML = 'Result: ' + result;
+};
+
+var isPrime = function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+  var i = 2;
+  while (i <= num / 2) {
+    if (num % i === 0) {
+      return false;
+    }
+    i++;
+  }
+  return true;
+};
+
+// Task 2
+// Организовать массив для хранения товаров в корзине
+var card = [];
+var item1 = { itemID: '1', name: 'book', price: 100, quantity: 1 };
+var item2 = { itemID: '2', name: 'pen', price: 5, quantity: 1 };
+
+var items = {
+  '1': { name: 'book', price: 100, quantity: 1 },
+  '2': { name: 'pen', price: 5, quantity: 1 }
+};
+
+var addItem = function addItem(art) {
+
+  if (art in card) {
+    card[art].quantity++;
+    console.log('card name: ' + card[art].name + '; card quantity: ' + card[art].quantity);
+  } else {
+    card[art] = items[art];
+    console.log('card name: ' + card[art].name + '; quantity: ' + card[art].quantity + '; card.length: ' + card.length);
+  }
+};
+
+// Показать содержимое корзины
+var getAnswerTask2 = function getAnswerTask2() {
+
+  var info = card.length === 0 ? 'empty' : '';
+  /*  for (let i = 0; i < card.length; i++) {
+      info = info + `<br>item ${i + 1}: ${card[i].name};
+      price: ${card[i].price};
+      quantity: ${card[i].quantity}`;
+    }*/
+  var i = 1;
+  for (var key in card) {
+    info = info + ('<br>item ' + i + ': ' + card[key].name + ';\n      price: ' + card[key].price + ';\n      quantity: ' + card[key].quantity);
+    i++;
+  }
+  document.getElementById('task2').innerHTML = 'Your Card: ' + info;
+};
+
+// Task 3
+// Организовать функцию countBasketPrice, которая будет считать стоимость корзины
+
+var countBasketPrice = function countBasketPrice(arr) {
+  var count = 0;
+  for (var key in arr) {
+    count = count + arr[key].price * arr[key].quantity;
+  }
+  return count;
+};
+
+// Показать стоимость корзины
+var getAnswerTask3 = function getAnswerTask3() {
+  document.getElementById('task3').innerHTML = 'Total price: ' + countBasketPrice(card);
+};
+
+// Task 4
+// Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть это должно так:
+// for(...){// здесь пусто}
+var getAnswerTask4 = function getAnswerTask4() {
+  for (var i = 0; i < 9; i++, alert('' + i)) {}
+};
+
+// Task 5
+// Нарисовать пирамиду с 20 рядами с помощью console.log
+var getAnswerTask5 = function getAnswerTask5() {
+  var item = '';
+
+  for (var i = 0; i < 20; i++) {
+    item += 'x';
+    console.log(item);
+  }
+};
--- src/lesson3.js
+++ src/lesson3.js
@@ -57,3 +57,105 @@ const compareArrs = (a, b) => {
   }
   
 };
+
+// Task 2
+// С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
+const getAnswerTask1 = () => {
+  let n = 0;
+  let result = [];
+  while (n <= 100) {
+    if (isPrime(n)) {
+      result = [...result, n];
+    } 
+    n++;
+  }
+  document.getElementById('task1').innerHTML = `Result: ${result}`;
+};
+
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  let i = 2;
+  while (i <= (num / 2)) {
+    if (num % i === 0) {
+      return false;
+    }
+    i++;
+  }
+  return true;
+};
+
+// Task 2
+// Организовать массив для хранения товаров в корзине
+const card = [];
+const item1 = {itemID: '1', name: 'book', price: 100, quantity: 1};
+const item2 = {itemID: '2', name: 'pen', price: 5, quantity: 1};
+
+const items = {
+  '1': {name: 'book', price: 100, quantity: 1},
+  '2': {name: 'pen', price: 5, quantity: 1}
+};
+
+const addItem = (art) => {
+
+
+        if (art in card) {
+          card[art].quantity++;
+          console.log(`card name: ${card[art].name}; card quantity: ${card[art].quantity}`);
+        } else {      
+            card[art] = items[art];
+            console.log(`card name: ${card[art].name}; quantity: ${card[art].quantity}; card.length: ${card.length}`);
+      }      
+   
+};
+
+
+// Показать содержимое корзины
+const getAnswerTask2 = () => {
+ 
+  let info = (card.length === 0) ? 'empty' : '';
+    let i = 1;
+    for( var key in card ) {
+      info = info + `<br>item ${i}: ${card[key].name};
+      price: ${card[key].price};
+      quantity: ${card[key].quantity}`;
+      i++;
+    }
+  document.getElementById('task2').innerHTML = `Your Card: ${info}`;
+};
+
+// Task 3
+// Организовать функцию countBasketPrice, которая будет считать стоимость корзины
+
+const countBasketPrice = (arr) => {
+  let count = 0;
+  for( var key in arr ) {
+    count = count + arr[key].price * arr[key].quantity;
+  }
+  return count;  
+};
+
+// Показать стоимость корзины
+const getAnswerTask3 = () => {
+  document.getElementById('task3').innerHTML = `Total price: ${countBasketPrice(card)}`;
+};
+
+// Task 4
+// Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть это должно так:
+// for(...){// здесь пусто}
+const getAnswerTask4 = () => {
+  for (let i = 0; i < 9; i++, alert(`${i}`)) {}
+};
+
+// Task 5
+// Нарисовать пирамиду с 20 рядами с помощью console.log
+const getAnswerTask5 = () => {
+  let item = '';
+
+  for (let i = 0; i < 20; i++) {
+    item += 'x';
+    console.log(item);
+  }
+};
